fa267025bbed8d05d67153a8c35ba4880fcd02b0
yarickprih <yarick.prih2903@hotmail.com>
2021-11-03 15:54:41 +0200

    created tests folder

---
+++ tests/conftest.py
@@ -0,0 +1,12 @@
+import pytest
+# from project import create_app
+
+
+# @pytest.fixture(scope="module")
+# def test_client():
+#     flask_app = create_app()
+#     testing_client = flask_app.test_client()
+#     ctx = flask_app.app_context()
+#     ctx.push()
+#     yield testing_client
+#     ctx.pop()
